deda02424cdd0176a3d68f1e0fba41ce230df9ae
jamiekim828 <jamiekim828@gmail.com>
2023-01-25 12:33:56 +0100

    test url

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 
 const requestListener = function (req: IncomingMessage, res: ServerResponse) {
-  if (req.method === 'GET' && req.url === '') {
+  if (req.method === 'GET' && req.url === '/users') {
     res.statusCode = 200;
     res.write('user list');
     res.end();
-  } else if (req.method === 'POST' && req.url === '') {
+  } else if (req.method === 'POST' && req.url === '/products') {
     res.write('create a new product');
     res.end();
-  } else if (req.method === 'DELETE' && req.url === '') {
-    res.write('delete user by id');
+  } else if (req.method === 'DELETE' && req.url === `/users/1`) {
+    res.write(`delete user by 1`);
     res.end();
   }
   res.writeHead(404);
